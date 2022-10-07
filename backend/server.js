b3967d1c9f2c4c4c7c42dffb84f03d2f9db88ba7
cybo-neutron <cyboneutron@gmail.com>
2022-10-07 16:12:12 +0530

    basic routes and schema

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const connectToMongo = require("./db");
 
-const port = 13172;
+const port = 13712;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+//Available routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
